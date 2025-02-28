ba99cfb8bcb53956567210e8a80ac4f466e65141
minsepar <p315315@gmail.com>
2025-02-28 17:42:45 +0900

    .

--- outer_circle/libasm/srcs/ft_write.asm
+++ outer_circle/libasm/srcs/ft_write.asm
@@ -4,26 +4,31 @@ section .text
     global ft_write
     ft_write:
         mov rax, 1
+    syscall
+    cmp rax, 0
+    jl .error
+    jmp .done
 %else
     extern ___error
     global _ft_write
     _ft_write:
         mov rax, 0x2000004  ; rdi = fd, rsi = buf, rdx = count
-%endif
     syscall
-
-    cmp rax, 0
-    jl .error
+    jc .error
     jmp .done
+%endif
 
 .error:
-    neg rax
+    sub rsp, 8
     mov r8, rax
     %ifdef OS_LINUX
+        neg r8
         call __errno_location wrt ..plt
     %else
         call ___error
     %endif
+    mov [rax], r8d
+    add rsp, 8
     mov [rax], r8
 
     mov rax, -1 ; return -1 on error
