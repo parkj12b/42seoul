383d108e0b73b80c05e5dec0b1c9eced876cd74b
minsepar <p315315@gmail.com>
2025-03-01 22:45:55 +0900

    .

--- outer_circle/libasm/srcs/ft_list_push_front_bonus.asm
+++ outer_circle/libasm/srcs/ft_list_push_front_bonus.asm
@@ -3,31 +3,22 @@ extern _ft_create_elem
 
 global _ft_list_push_front
 _ft_list_push_front:
-    push    rbp             ; Save rbp (callee-saved)
-    mov     rbp, rsp        ; Set up stack frame
-    push    rbx             ; Save rbx (callee-saved)
+    push    rdi             ; Save rdi (t_list **begin_list)
+    mov     rdi, rsi        ; Pass rsi (void *data)
+    sub     rsp, 8          ; Align stack
+    call    _ft_create_elem
 
-    mov     rbx, rdi        ; Save t_list **begin_list (rdi) in rbx
-    mov     rdi, rsi        ; Pass void *data (rsi) to _ft_create_elem
+    add     rsp, 8
+    test    rax, rax
+    jz      .error
+    pop     rsi             ; Restore t_list **begin_list into rsi
 
-    push    rdi
-    push    rsi
+    mov     rdi, [rsi]      ; Get current head
+    mov     [rax + 8], rdi
+    mov     [rsi], rax      ; Update head
 
-    sub     rsp, 8          ; Align stack (16-byte boundary with return address)
-    call    _ft_create_elem ; Create new node, returns in rax
-    add     rsp, 8          ; Restore stack
+    ret
 
-    pop rsi
-    pop rdi
-    test    rax, rax        ; Check if creation failed
-    jz      .done
-
-    mov     rsi, [rbx]      ; Get current head (*begin_list)
-    mov     [rax + 8], rsi  ; new_node->next = current head
-    mov     [rbx], rax      ; *begin_list = new_node
-
-.done:
-    pop     rbx             ; Restore rbx
-    mov     rsp, rbp        ; Restore stack pointer
-    pop     rbp             ; Restore rbp
+.error:
+    add     rsp, 8          ; Balance stack
     ret
